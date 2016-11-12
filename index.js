12f7cf3b29477d03d0edf799590cdd80f3e0482f
Sergey136 <se63@yandex.ru>
2016-11-12 13:43:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', async(req,res) => {
+	
+const re=RegExp('([а-я]*)([ ]+)([а-я]*)([ ]+)([а-я]*)','i');
+const re2=RegExp('([а-я]*)([ ]+)([а-я]*)','i');
+const re3=RegExp('([а-я]*)','i');
+const re4=RegExp('([а-я]*)([ ]+)([а-я]*)([ ]+)([а-я]*)([ ]+)([а-я]*)','i');
+	var full = req.query.fullname;
+	var fuller = "";
+const reini = RegExp('([а-яa-z])');
+	var fam ="фамилия";
+	var name = "имя";
+	var otch = "отчество";
+	var output = "fio";
+	var words = full.split(" ").length;
+		if(words<4){
+	if(re.test(full)){
+		fuller = full.match(re);
+		fam = fuller[5];
+		name = fuller[1].match(reini);
+		otch = fuller[3].match(reini);
+		output = fam+' '+name[1]+'.'+' '+otch[1]+'.';
+	}else if(re2.test(full)){
+		fuller = full.match(re2);
+		fam = fuller[3];
+		name = fuller[1].match(reini);
+		otch = "";
+		output = fam+' '+name[1]+'.';
+	}else if(re3.test(full)){
+		fuller = full.match(re3);
+		fam = fuller[1];
+		name = "";
+		otch = "";
+		output = fam;
+	}
+		}else{
+		output = "invalid fulname";
+	}
+	
+	return res.json({
+		fio:output,
+		full:full,
+		fuller:fuller,
+		words:words,
+	});
+
+});
+
+app.listen(3000,() => {
+  console.log('Example app listening on port 3000!');
+});
